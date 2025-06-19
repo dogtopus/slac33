27f8f29bcc4f25d721753b7b601bc9fa39e4f336
dogtopus <dogtopus@users.noreply.github.com>
2025-06-19 22:22:22 +0900

    Implement R_C33_D* and R_C33_S_R* relocation types

--- src/main/java/ghidra/app/util/bin/format/elf/relocation/S1C33_ElfRelocationHandler.java
+++ src/main/java/ghidra/app/util/bin/format/elf/relocation/S1C33_ElfRelocationHandler.java
@@ -12,6 +12,7 @@ import ghidra.program.model.address.Address;
 import ghidra.program.model.listing.Program;
 import ghidra.program.model.mem.Memory;
 import ghidra.program.model.mem.MemoryAccessException;
+import ghidra.program.model.mem.MemoryBlock;
 import ghidra.program.model.reloc.Relocation.Status;
 import ghidra.program.model.reloc.RelocationResult;
 
@@ -35,27 +36,63 @@ public class S1C33_ElfRelocationHandler extends AbstractElfRelocationHandler<S1C
 		Program program = elfRelocationContext.getProgram();
 		Memory mem = program.getMemory();
 
-		// final MessageLog log = elfRelocationContext.getLog();
+		final MessageLog log = elfRelocationContext.getLog();
 
 		switch (relocationType) {
 		case R_C33_NONE:
 			return RelocationResult.SKIPPED;
 		case R_C33_DH: {
-			long dpRel = calculateDPRel(symbolValue);
+			Long dpRel = calculateDPRel(mem, symbolValue);
+			if (dpRel == null) {
+				return RelocationResult.FAILURE;
+			}
 			short original = mem.getShort(relocationAddress);
 			short patched = (short) ((original & 0xe000) | ((dpRel >> 13) & 0x1fff));
 			mem.setShort(relocationAddress, patched);
 			return new RelocationResult(Status.APPLIED, 2);
 		}
 		case R_C33_DL: {
-			long dpRel = calculateDPRel(symbolValue);
+			Long dpRel = calculateDPRel(mem, symbolValue);
+			if (dpRel == null) {
+				return RelocationResult.FAILURE;
+			}
 			short original = mem.getShort(relocationAddress);
 			short patched = (short) ((original & 0xe000) | (dpRel & 0x1fff));
 			mem.setShort(relocationAddress, patched);
 			return new RelocationResult(Status.APPLIED, 2);
 		}
+		case R_C33_S_RH: {
+			Long pcrel = calculatePCRel(relocationAddress, relocationType, symbolValue, symbolName, log);
+			if (pcrel == null) {
+				return RelocationResult.FAILURE;
+			}
+			short original = mem.getShort(relocationAddress);
+			short patched = (short) ((original & 0xe000) | ((pcrel >> 22) & 0x3ff));
+			mem.setShort(relocationAddress, patched);
+			return new RelocationResult(Status.APPLIED, 2);
+		}
+		case R_C33_S_RM: {
+			Long pcrel = calculatePCRel(relocationAddress, relocationType, symbolValue, symbolName, log);
+			if (pcrel == null) {
+				return RelocationResult.FAILURE;
+			}
+			short original = mem.getShort(relocationAddress);
+			short patched = (short) ((original & 0xe000) | ((pcrel >> 9) & 0x1fff));
+			mem.setShort(relocationAddress, patched);
+			return new RelocationResult(Status.APPLIED, 2);
+		}
+		case R_C33_S_RL: {
+			Long pcrel = calculatePCRel(relocationAddress, relocationType, symbolValue, symbolName, log);
+			if (pcrel == null) {
+				return RelocationResult.FAILURE;
+			}
+			short original = mem.getShort(relocationAddress);
+			short patched = (short) ((original & 0xff00) | ((pcrel >> 1) & 0xff));
+			mem.setShort(relocationAddress, patched);
+			return new RelocationResult(Status.APPLIED, 2);
+		}
 		default:
-			return RelocationResult.FAILURE;
+			return RelocationResult.UNSUPPORTED;
 		}
 	}
 
@@ -64,7 +101,36 @@ public class S1C33_ElfRelocationHandler extends AbstractElfRelocationHandler<S1C
 		return elf.e_machine() == ElfConstants.EM_SE_C33;
 	}
 
-	private static long calculateDPRel(long symbolValue) {
-		return symbolValue;
+	private static Long calculateDPRel(Memory mem, long symbolValue) {
+		MemoryBlock dBlock = mem.getBlock(".rodata");
+		if (dBlock == null) {
+			return null;
+		}
+		long dBase = dBlock.getStart().getOffset();
+		return symbolValue - dBase;
+	}
+	
+	private static Long calculatePCRel(
+			Address relocationAddress,
+			S1C33_ElfRelocationType relocationType,
+			long symbolValue,
+			String symbolName,
+			MessageLog log
+	) {
+		if ((symbolValue & 1) != 0) {
+			log.appendMsg(String.format("Symbol %s has invalid address %08x.", symbolName, symbolValue));
+			return null;
+		}
+		long offset;
+		if (relocationType == S1C33_ElfRelocationType.R_C33_S_RH) {
+			offset = 4;
+		} else if (relocationType == S1C33_ElfRelocationType.R_C33_S_RM) {
+			offset = 2;
+		} else {
+			offset = 0;
+		}
+		Address presumedInstOffset = relocationAddress.add(offset);
+		long pcrel = symbolValue - presumedInstOffset.getOffset();
+		return pcrel;
 	}
 }
